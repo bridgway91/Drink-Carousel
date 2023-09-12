749a041fa7313784bc50906e0f690003606d37a5
Brandon Ridgway <bridgway91@gmail.com>
2023-09-11 21:57:39 -0400

    got some js done, still having trouble with adding event listener to created objects

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-document.querySelector('button').addEventListener('click', findDrink);
-document.querySelector('body').addEventListener('click', function(event) {
-    if (event.target.tagName.toLowerCase() === 'li') {
-      // do your action on your 'li' or whatever it is you're listening for
-      console.log(event.target);
-    }
-});
+document.querySelector('#search').addEventListener('click', findDrink);
+document.querySelector('body').addEventListener('click', pickDrink);
+/*
+// document.querySelector('body').addEventListener('click', function(event) {
+//     if (event.target.tagName.toLowerCase() === 'li') {
+//       // do your action on your 'li' or whatever it is you're listening for
+//       console.log(event.target);
+//     }
+// });
+*/
 
 
 function findDrink() {
     let drink = document.querySelector('input').value;
+    let carousel = document.querySelector('#results');
 
     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+drink)
         .then(res => res.json())
         .then(data => {
-            let target = data.drinks
-            document.getElementById('drinks-list').innerHTML = ''
-            target.forEach(el => {
-                document.getElementById('drinks-list').innerHTML += (`<div><img src="${el.strDrinkThumb}">
-                <span>${el.strDrink}</span></div>`);
+            let drinklist = data.drinks;
+            console.log(drinklist)
+            carousel.innerHTML = '';
+            drinklist.forEach((el,index) => {
+                carousel.innerHTML += (
+                    `<button id="result-${index}" type="button">
+					<img src="${el.strDrinkThumb}">
+					<span>${el.strDrink}</span>
+				    </button>`
+                );
             });
+            // document.querySelector(`#results`).button.addEventListener('click', pickDrink);
+            /* concerning cloning nodes for carousel
             // let copy = document.querySelector('ul').cloneNode(false);
             // document.querySelector('ul').appendChild(copy);
 
             // document.querySelector('h2').innerText = target.strDrink
             // document.querySelector('img').src = target.strDrinkThumb
             // document.querySelector('h3').innerText = target.strInstructions
+            */
         })
         .catch(err => {
             console.log(`error ${err}`)
@@ -33,7 +45,7 @@ function findDrink() {
 }
 
 function pickDrink() {
-    console.log(target);
+    console.log(event.target.tagName);
 }
 /*
 hw:
